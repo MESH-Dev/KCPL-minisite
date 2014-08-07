439150c5531288deac7adda0efda7724da011bbe
robbiesh <sherrard.rob@gmail.com>
2014-08-07 14:11:15 -0400

    footer styled

--- wp-content/themes/kcpl_minisite/footer.php
+++ wp-content/themes/kcpl_minisite/footer.php
@@ -1,7 +1,9 @@
 <footer>
-  <span class="footer-title">Thank you and some kind of take-away??</span>
-  <span class="footer-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-  <span class="footer-url">www.url.com</span>
+    <div class="footer-text">
+      <span class="footer-title">Thank you and some kind of take-away??</span>
+      <span class="footer-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+      <span class="footer-url">www.url.com</span>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
