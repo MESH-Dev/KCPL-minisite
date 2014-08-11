6bdbcd0d8f3f9469704ed10ff21fb6c73807545f
robbiesh <sherrard.rob@gmail.com>
2014-08-11 13:09:50 -0400

    more text changes

--- wp-content/themes/kcpl_minisite/footer.php
+++ wp-content/themes/kcpl_minisite/footer.php
@@ -2,7 +2,7 @@
     <div class="footer-text">
       <span class="footer-title">Thank you and some kind of take-away??</span>
       <span class="footer-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
-      <span class="footer-url">www.url.com</span>
+      <span class="footer-url">www.kanawhalibrary.org</span>
     </div>
 </footer>
 <?php wp_footer(); ?>
